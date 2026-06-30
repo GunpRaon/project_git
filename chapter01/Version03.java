c4d4ac90097ae087a967bcd4b19b963ea78f4a55
raon <raonraong@naver.com>
2026-06-30 11:55:50 +0900

    version control5_B

--- chapter01/Version03.java
+++ chapter01/Version03.java
@@ -3,7 +3,8 @@ package chapter01;
 public class Version03 {
 
     public Version03(){
-        System.out.println();
+        System.out.println("A");
+        System.out.println("B");
     }
     
 }
